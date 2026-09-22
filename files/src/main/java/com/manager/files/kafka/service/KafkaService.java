a026aad42dda7ae56bf00e4982ceebb355a468a5
Sunny Kasera <sunnykasera3107@gmail.com>
2026-09-22 18:07:40 +0530

    Adding kafka

--- files/src/main/java/com/manager/files/kafka/service/KafkaService.java
+++ files/src/main/java/com/manager/files/kafka/service/KafkaService.java
@@ -19,6 +19,23 @@ public class KafkaService {
         kafkaTemplate.send(
             KafkaTopics.FILE_PROCESSING, 
             fileId
-        );
+        ).whenComplete((result, exception) -> {
+
+            if (exception != null) {
+                System.out.println("❌ Kafka send failed");
+                exception.printStackTrace();
+            } else {
+                System.out.println("✅ Kafka send successful");
+                System.out.println(
+                    "Topic: " + result.getRecordMetadata().topic()
+                );
+                System.out.println(
+                    "Partition: " + result.getRecordMetadata().partition()
+                );
+                System.out.println(
+                    "Offset: " + result.getRecordMetadata().offset()
+                );
+            }
+        });
     }
 }
