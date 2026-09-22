81bd4d9c8328a2cbba94c2d96b374b46ac00e2b4
Sunny Kasera <sunnykasera3107@gmail.com>
2026-09-22 18:09:49 +0530

    Adding kafka

--- files/src/main/java/com/manager/files/config/KafkaProducerConfig.java
+++ files/src/main/java/com/manager/files/config/KafkaProducerConfig.java
@@ -23,7 +23,7 @@ public class KafkaProducerConfig {
 
         config.put(
             ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-            "localhost:9092"
+            "kafka:9092"
         );
 
         config.put(
