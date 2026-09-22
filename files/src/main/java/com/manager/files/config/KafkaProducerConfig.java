b0a80833e79bc1806233469a5101f94748d9bb2f
Sunny Kasera <sunnykasera3107@gmail.com>
2026-09-22 17:36:17 +0530

    Adding kafka

---
+++ files/src/main/java/com/manager/files/config/KafkaProducerConfig.java
@@ -0,0 +1,48 @@
+package com.manager.files.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import com.nimbusds.jose.shaded.gson.JsonSerializer;
+
+@Configuration 
+public class KafkaProducerConfig {
+
+    @Bean 
+    public ProducerFactory<String, String> producerFactory() {
+
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+            "localhost:9092"
+        );
+
+        config.put(
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+            StringSerializer.class
+        );
+
+        config.put(
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+            JsonSerializer.class
+        );
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(
+            ProducerFactory<String, String> producerFactory) {
+
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
