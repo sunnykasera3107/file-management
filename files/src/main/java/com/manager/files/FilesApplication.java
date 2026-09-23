847eeb9c0d905e38a6502df34616a3fe871a5bf1
Sunny Kasera <sunnykasera3107@gmail.com>
2026-09-23 21:37:16 +0530

    Testing kafka

--- files/src/main/java/com/manager/files/FilesApplication.java
+++ files/src/main/java/com/manager/files/FilesApplication.java
@@ -2,9 +2,7 @@ package com.manager.files;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
-@EnableKafka 
 @SpringBootApplication
 public class FilesApplication {
 
--- files/src/main/java/com/manager/files/config/KafkaProducerConfig.java
+++ files/src/main/java/com/manager/files/config/KafkaProducerConfig.java
@@ -7,8 +7,6 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -46,16 +44,4 @@ public class KafkaProducerConfig {
         return new KafkaTemplate<>(producerFactory);
     }
 
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String>
-            kafkaListenerContainerFactory(
-                ConsumerFactory<String, String> consumerFactory) {
-
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-            new ConcurrentKafkaListenerContainerFactory<>();
-
-        factory.setConsumerFactory(consumerFactory);
-
-        return factory;
-    }
 }
