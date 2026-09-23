4f7d2524261377f6b810f692ad7d68a302f46525
Sunny Kasera <sunnykasera3107@gmail.com>
2026-09-23 21:32:44 +0530

    Testing kafka

--- files/src/main/java/com/manager/files/FilesApplication.java
+++ files/src/main/java/com/manager/files/FilesApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka 
 @SpringBootApplication
 public class FilesApplication {
 
